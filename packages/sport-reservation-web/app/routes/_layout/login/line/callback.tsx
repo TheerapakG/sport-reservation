11004057aba18f75eb298af44ce6f7834d68012e
TheerapakG <theerapakg@gmail.com>
2025-01-24 22:44:38 +0700

    fix: less eager auth callback

--- packages/sport-reservation-web/app/routes/_layout/login/line/callback.tsx
+++ packages/sport-reservation-web/app/routes/_layout/login/line/callback.tsx
@@ -1,10 +1,36 @@
 import { authKeys, lineLoginAuthTokenQueryOptions } from "@/api/auth";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { useQueryClient } from "@tanstack/react-query";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { type } from "arktype";
 import { Effect } from "effect";
+import { use } from "react";
 import { effectTypeCheck } from "sport-reservation-common/utils/effectType";
 
+const authAndRedirect = async ({
+  queryClient,
+  router,
+  code,
+  state,
+}: {
+  queryClient: ReturnType<typeof useQueryClient>;
+  router: ReturnType<typeof useRouter>;
+  code: string;
+  state: string;
+}) => {
+  await queryClient.fetchQuery(lineLoginAuthTokenQueryOptions({ code, state }));
+  await queryClient.invalidateQueries({
+    queryKey: authKeys.token.all(),
+  });
+  await router.navigate({ to: "/" });
+};
+
 const CallbackComponent = () => {
+  const { code, state } = Route.useLoaderData();
+
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const _ = use(authAndRedirect({ queryClient, router, code, state }));
+
   return (
     <div className="p-2">
       <h3>Redirecting...</h3>
@@ -17,17 +43,8 @@ const validateSearch = type({ code: "string", state: "string" });
 export const Route = createFileRoute("/_layout/login/line/callback")({
   validateSearch,
   loaderDeps: ({ search }) => search,
-  loader: async ({ deps, context: { queryClient } }) => {
-    const { code, state } = Effect.runSync(effectTypeCheck(deps));
-    await queryClient.ensureQueryData(
-      lineLoginAuthTokenQueryOptions({ code, state }),
-    );
-    await queryClient.invalidateQueries({
-      queryKey: authKeys.token.all(),
-    });
-    throw redirect({
-      to: "/",
-    });
+  loader: async ({ deps }) => {
+    return Effect.runSync(effectTypeCheck(deps));
   },
   component: CallbackComponent,
 });
