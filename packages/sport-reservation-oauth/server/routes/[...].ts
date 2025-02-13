2f35d27618eb68d0b0a60400a1d2b94256747473
TheerapakG <theerapakg@gmail.com>
2025-02-13 10:54:26 +0700

    chore: debug

--- packages/sport-reservation-oauth/server/routes/[...].ts
+++ packages/sport-reservation-oauth/server/routes/[...].ts
@@ -1,25 +1,26 @@
 import { EventContext, effectEventHandler } from "$/effectEventHandler";
 import { type } from "arktype";
-import { Console, Effect } from "effect";
+import { Effect } from "effect";
 import { sendWebResponse, toWebRequest } from "h3";
 import { defineEventHandlerConfig } from "tiara-stack/config";
 import { Issuer } from "../layers/issuer";
 
 export const handlerConfig = defineEventHandlerConfig({
   name: "oauth",
-  response: type("unknown"),
+  response: type("undefined"),
 });
 export default effectEventHandler({
   config: handlerConfig,
   handler: /*@__PURE__*/ Effect.gen(function* () {
-    yield* Console.log("handler");
+    console.log("handler");
     const { event } = yield* EventContext;
     const request = toWebRequest(event);
-    yield* Console.log(request);
+    console.log(request);
     const { issuer } = yield* Issuer;
     const response = yield* Effect.promise(
       async () => await issuer.fetch(request),
     );
     yield* Effect.promise(async () => await sendWebResponse(event, response));
+    return undefined;
   }),
 });
