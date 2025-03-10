4524585d628b448421ef71fbea8db2b7560857d6
TheerapakG <theerapakg@gmail.com>
2025-03-10 20:25:02 +0700

    fix: test stack handler

--- packages/tiara-stack-test/server/routes/test-stream.get.ts
+++ packages/tiara-stack-test/server/routes/test-stream.get.ts
@@ -1,6 +1,6 @@
 import { effectEventHandler } from "$/effectEventHandler";
 import { type } from "arktype";
-import { Console, Stream } from "effect";
+import { Console, Effect, Stream } from "effect";
 import { defineEventHandlerConfig, response } from "tiara-stack/config";
 
 export const handlerConfig = defineEventHandlerConfig({
@@ -9,10 +9,12 @@ export const handlerConfig = defineEventHandlerConfig({
 });
 
 export default /*@__PURE__*/ effectEventHandler(handlerConfig)((streamSource) =>
-  streamSource.pipe(
-    Stream.haltAfter("1 seconds"),
-    Stream.concat(Stream.drain(Stream.finalizer(Console.log("finalize")))),
-    Stream.tap(() => Console.log(new Date())),
-    Stream.flatMap(() => Stream.succeed({ test: "test" })),
+  Effect.succeed(
+    streamSource.pipe(
+      Stream.haltAfter("1 seconds"),
+      Stream.concat(Stream.drain(Stream.finalizer(Console.log("finalize")))),
+      Stream.tap(() => Console.log(new Date())),
+      Stream.flatMap(() => Stream.succeed({ test: "test" })),
+    ),
   ),
 );
